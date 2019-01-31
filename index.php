42a047f5c251ec4c7d5bdb3dbe52bc39a122a8d6
Craig Burgess <craig@geniusdivision.com>
2019-01-31 11:22:58 +0000

    Added first version of SCSS boilerplate

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SavePoint&trade;</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="/assets/css/screen.css" />
 </head>
 <body>
     <h1>SavePoint&trade;</h1>
