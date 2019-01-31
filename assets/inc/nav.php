c77af29416a14d9a34296357fae131bb38217d97
Craig Burgess <craig@geniusdivision.com>
2019-01-31 15:12:12 +0000

    Updated grid framework with thanks to a little research

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -5,9 +5,9 @@
 */
 ?>
 
-<ul class="nav">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Gallery</a></li>
-    <li><a href="#">Contact</a></li>
-</ul>
+            <ul class="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
--- index.php
+++ index.php
@@ -27,13 +27,24 @@ require_once('assets/inc/header.php');
             <div class="col-8">
                 <div class="content__main">
                     <h1>SavePoint&trade;</h1>
+
+                    <p>Originally created for our students, but <em>free for all</em>.</p>
+
+                    <p>SavePoint&trade; is a very simple front-end starter boilerplate that we created to introduce a few core concepts to our students:</p>
+
+                    <ol>
+                        <li>PHP includes and basic PHP syntax.</li>
+                        <li>SCSS basic syntax and compiling with a third party app.</li>
+                        <li>The power of creating your own boilerplate and not having to start from scratch every time you work on a new project.</li>
+                    </ol>
                 </div>
                 <!-- /.content__main -->
             </div>
             <!-- /.col-8 -->
             <div class="col-4">
                 <div class="content__sidebar">
-                    <h2>Sidebar</h2>
+                    <h2>Recommendations</h2>
+                    <p>We recommend you spend a bit of time learning how git works. It isn't required to use SavePoint&trade;, but you'll be expected to know it when you get a job. Have a read through GitHub's guide about their workflow, and this excellent resource from @blackfalcon</p>
                 </div>
                 <!-- /.content__sidebar -->
             </div>
